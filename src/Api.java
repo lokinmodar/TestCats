278458b27b85f79bcc480c26f5c526c0d191ba14
Dante Souza e Souza <lokinmodar@gmail.com>
2017-11-25 13:35:46 -0200

    Async API and Helper
    Adjusting code to match Async call logic

--- src/Api.java
+++ src/Api.java
@@ -3,7 +3,14 @@ import com.sun.jndi.toolkit.url.Uri;
 import java.util.List;
 
 public interface Api {
-    List<Cat> queryCats(String query);
+    interface CatsQueryCallback{
+        void onCatListReceived(List<Cat> cats);
+        void onError(Exception e);
+    }
+
+    void queryCats(String query, CatsQueryCallback catsQueryCallback);
+
+
     Uri store(Cat cat);
 
 }
--- src/CatsHelper.java
+++ src/CatsHelper.java
@@ -1,18 +1,37 @@
 import com.sun.jndi.toolkit.url.Uri;
 
+import java.security.PublicKey;
 import java.util.Collections;
 import java.util.List;
 
 public class CatsHelper {
+
+    public interface CutestCatCallback{
+        void onCutestCatSaved(Uri uri);
+        void onQueryFailed(Exception e);
+    }
+
     Api api;
 
-    public Uri saveTheCutestCat(String query){
-        List<Cat> cats = api.queryCats(query);
-        Cat cutest = findCutest(cats);
-        Uri savedUrl = api.store(cutest);
-        return savedUrl;
+    public void saveTheCutestCat(String query, CutestCatCallback cutestCatCallback){
+        api.queryCats(query, new Api.CatsQueryCallback(){
+            @Override
+            public void onCatListReceived(List<Cat> cats) {
+                Cat cutest = findCutest(cats);
+                Uri savedUri = api.store(cutest);
+                cutestCatCallback.onCutestCatSaved(savedUri);
+            }
 
+            @Override
+            public void onError(Exception e) {
+                cutestCatCallback.onQueryFailed(e);
+
+            }
+
+
+        });
     }
+
 
     private Cat findCutest(List<Cat> cats) {
         return Collections.max(cats);
